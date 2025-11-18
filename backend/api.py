ae3df23a5c6eb363d05bfc879b215494cf446ac6
Daniel <duran149@csusm.edu>
2025-11-18 14:36:13 -0800

    adding everything

--- backend/api.py
+++ backend/api.py
@@ -1,1 +1,13 @@
-print("HEllo")
+from openai import OpenAI
+
+client = OpenAI(
+  api_key=""
+)
+
+response = client.responses.create(
+  model="gpt-5-nano",
+  input="write a haiku about ai",
+  store=True,
+)
+
+print(response.output_text)
